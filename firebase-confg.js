4acfe172250434b81bdbce6c9e0b1a1c07fa758c
Justine@mrnoone <justine095justine@gmail.com>
2026-09-04 07:51:24 +0300

    Update Firebase configuration for VijiApp

--- firebase-confg.js
+++ firebase-confg.js
@@ -1,13 +1,10 @@
-// Firebase Config ya VijiApp
 const firebaseConfig = {
-  apiKey: "WEKA_API_KEY_YAKO_HAPA",
-  authDomain: "vijiapp.firebaseapp.com",
-  databaseURL: "https://vijiapp-default-rtdb.firebaseio.com",
-  projectId: "vijiapp",
-  storageBucket: "vijiapp.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyBPtk66nvYMLGbeKZaeuXS1i_uhu-uOUYw",
+  authDomain: "viji-app-84c7d.firebaseapp.com",
+  projectId: "viji-app-84c7d",
+  storageBucket: "viji-app-84c7d.firebasestorage.app",
+  messagingSenderId: "906281008310",
+  appId: "1:906281008310:web:4616f79f3088cf43b3fa17"
 };
-
 // Anzisha Firebase
 firebase.initializeApp(firebaseConfig);
