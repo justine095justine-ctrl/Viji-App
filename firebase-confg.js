d4473e96d7b6e8306a48879b0606cc90facb2c95
Justine@mrnoone <justine095justine@gmail.com>
2026-09-04 07:38:39 +0300

    Initialize Firebase with configuration settings
    Add Firebase configuration for VijiApp.

---
+++ firebase-confg.js
@@ -0,0 +1,13 @@
+// Firebase Config ya VijiApp
+const firebaseConfig = {
+  apiKey: "WEKA_API_KEY_YAKO_HAPA",
+  authDomain: "vijiapp.firebaseapp.com",
+  databaseURL: "https://vijiapp-default-rtdb.firebaseio.com",
+  projectId: "vijiapp",
+  storageBucket: "vijiapp.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+// Anzisha Firebase
+firebase.initializeApp(firebaseConfig);
